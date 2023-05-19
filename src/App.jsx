f26f1e7e36b7b4bca4034ffeb648509f765c1e4c
Wakana <wakaichi@gmail.com>
2023-05-19 13:09:07 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="main w-screen bg-[#152938]"></div>
+
+    </>
+  )
+}
+
+export default App
